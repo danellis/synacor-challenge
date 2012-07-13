3caddba98a38f4b5be18ff6fecfbc713a81ab6a6
Dan Ellis <dan@electricwords.org>
2012-07-13 00:23:27 -0400

    Passes self-test

--- vm.py
+++ vm.py
@@ -2,6 +2,14 @@
 import sys, itertools
 from array import array
 
+CHARS = {
+    0: 'NUL', 1: 'SOH', 2: 'STX', 3: 'ETX', 4: 'EOT', 5: 'ENQ', 6: 'ACK', 7: 'BEL',
+    8: 'BS', 9: 'HT', 10: 'NL', 11: 'VT', 12: 'NP', 13: 'CR', 14: 'SO', 15: 'SI',
+    16: 'DLE', 17: 'DC1', 18: 'DC2', 19: 'DC3', 20: 'DC4', 21: 'NAK', 22: 'SYN', 23: 'ETB',
+    24: 'CAN', 25: 'EM', 26: 'SUB', 27: 'ESC', 28: 'FS', 29: 'GS', 30: 'RS', 31: 'US',
+    32: 'SP'
+}
+
 class VmException(Exception): pass
 class VmHalted(VmException): pass
 class UndefinedOpcode(VmException): pass
@@ -26,32 +34,20 @@ class Memory(object):
 
     def __getitem__(self, address):
         try:
-            value = self.memory[address]
+            return self.memory[address]
         except IndexError:
             raise InvalidAddress("Address %s is invalid" % address)
-        return self.real_value(value)
 
     def __setitem__(self, address, value):
-        value = self.real_value(value)
-        try:
+        if 0 <= address <= 32767:
             self.memory[address] = value % 32768
-        except IndexError:
-            raise InvalidAddress("Address %s is invalid" % address)
+        elif 32768 <= address <= 32775:
+            self.registers[address - 32768] = value
+        else:
+            raise InvalidAddress("Address %s is neither memory nor register" % address)
 
     def read(self, start, length):
-        return map(self.real_value, self.memory[start:start + length])
-
-    def real_value(self, value):
-        if 0 <= value <= 32767:
-            return value
-
-        if 32768 <= value <= 32775:
-            reg = value - 32768
-            if reg > 7:
-                raise InvalidRegister("Register %s is invalid" % reg)
-            return self.registers[reg]
-
-        raise InvalidValue("Value %s is invalid as a number or a register" % value)
+        return self.memory[start:start + length]
 
 class Stack(object):
     def __init__(self):
@@ -90,98 +86,153 @@ class VirtualMachine:
         21: ('noop', 0),
     }
 
-    def __init__(self):
+    def __init__(self, trace=False):
         self.registers = RegisterBank()
         self.memory = Memory(self.registers)
         self.stack = Stack()
         self.pc = 0
+        self.trace = trace
 
     def load(self, filename):
-        code = array('H')
-        code.fromstring(file(filename, 'rb').read())
-        self.memory.memory[0:len(code)] = code
+        code_array = array('H')
+        code_str = file(filename, 'rb').read()
+        code_array.fromstring(code_str)
+        size = len(code_array)
+        self.memory.memory[0:size] = code_array
+        return size
 
     def execute(self):
         try:
             while 1:
-                # Get next operation
-                opcode = self.memory[self.pc]
-                try:
-                    op_name, num_args = self.ops[opcode]
-                except KeyError:
-                    raise UndefinedOpcode("Opcode %s not defined" % opcode)
-                op_fn = getattr(self, 'op_%s' % op_name)
+                op_name, args = self.fetch_instruction(self.pc)
 
-                # Read operands from memory following opcode
-                args = self.memory.read(self.pc + 1, num_args)
+                if self.trace:
+                    # Print disassembly of instruction
+                    sys.stderr.write('%s: %s    ; %s\n' % (
+                        self.pc,
+                        self.disassemble_instruction(op_name, args),
+                        ' '.join(map(str, self.memory.read(self.pc, 1 + len(args))))
+                    ))
 
                 # Execute instruction, and update PC with the returned address
                 # or the next instruction if None is returned
-                self.pc = op_fn(*args) or (self.pc + 1 + num_args)
+                op_fn = getattr(self, 'op_%s' % op_name)
+                self.pc += 1 + len(args)
+                op_fn(*args) or (self.pc + 1 + len(args))
+
+                if self.trace:
+                    sys.stderr.write('    reg: %s\n' % ' '.join(map(str, self.registers.registers)))
+                    sys.stderr.write('    stk: %s\n' % ' '.join(map(str, self.stack.stack)))
         except VmHalted:
             print "Halted"
 
+    def fetch_instruction(self, addr):
+        opcode = self.memory[addr]
+        try:
+            op_name, num_args = self.ops[opcode]
+        except KeyError:
+            raise UndefinedOpcode("Opcode %s not defined" % opcode)
+
+        # Read operands from memory following opcode
+        args = self.memory.read(addr + 1, num_args)
+        return op_name, args
+
+    def value(self, value):
+        if 0 <= value <= 32767:
+            return value
+
+        if 32768 <= value <= 32775:
+            return self.registers[value - 32768]
+
+        raise InvalidValue("Value %s is invalid as a number or a register" % value)
+
     def op_halt(self):
-        raise VmHalted("Halted")
+        raise VmHalted
 
     def op_set(self, reg, value):
-        self.registers[reg] = value
+        self.registers[reg - 32768] = self.value(value)
 
     def op_push(self, value):
+        value = self.value(value)
         self.stack.push(value)
 
     def op_pop(self, addr):
         self.memory[addr] = self.stack.pop()
 
     def op_eq(self, a, b, c):
+        b = self.value(b)
+        c = self.value(c)
         self.memory[a] = 1 if b == c else 0
 
     def op_gt(self, a, b, c):
+        b = self.value(b)
+        c = self.value(c)
         self.memory[a] = 1 if b > c else 0
 
     def op_jmp(self, addr):
-        return addr
+        addr = self.value(addr)
+        self.pc = addr
 
     def op_jt(self, a, addr):
+        a = self.value(a)
+        addr = self.value(addr)
         if a:
-            return addr
+            self.pc = addr
 
     def op_jf(self, a, addr):
+        a = self.value(a)
+        addr = self.value(addr)
         if a == 0:
-            return addr
+            self.pc = addr
 
     def op_add(self, a, b, c):
+        b = self.value(b)
+        c = self.value(c)
         self.memory[a] = b + c
 
     def op_mult(self, a, b, c):
+        b = self.value(b)
+        c = self.value(c)
         self.memory[a] = b * c
 
     def op_mod(self, a, b, c):
+        b = self.value(b)
+        c = self.value(c)
         self.memory[a] = b % c
 
     def op_and(self, a, b, c):
+        b = self.value(b)
+        c = self.value(c)
         self.memory[a] = b & c
 
     def op_or(self, a, b, c):
+        b = self.value(b)
+        c = self.value(c)
         self.memory[a] = b | c
 
     def op_not(self, a, b):
+        b = self.value(b)
         self.memory[a] = ~b
 
     def op_rmem(self, a, b):
+        b = self.value(b)
         self.memory[a] = self.memory[b]
 
     def op_wmem(self, a, b):
+        a = self.value(a)
+        b = self.value(b)
         self.memory[a] = b
 
     def op_call(self, addr):
-        self.stack.push(self.pc + 2)
-        return addr
+        addr = self.value(addr)
+        self.stack.push(self.pc)
+        self.pc = addr
 
     def op_ret(self):
-        return self.stack.pop()
+        self.pc = self.stack.pop()
 
     def op_out(self, char):
+        char = self.value(char)
         sys.stdout.write(chr(char))
 
     def op_in(self, addr):
@@ -190,7 +241,36 @@ class VirtualMachine:
     def op_noop(self):
         pass
 
+    def disassemble(self, addr, length):
+        end = addr + length
+        while addr < end:
+            try:
+                op_name, args = self.fetch_instruction(addr)
+                print '%s: %s' % (addr, self.disassemble_instruction(op_name, args))
+                addr += 1 + len(args)
+            except UndefinedOpcode:
+                print '%s: ???' % addr
+                addr += 1
+
+    def disassemble_instruction(self, op_name, args):
+        return '%s %s' % (
+            op_name,
+            ', '.join(map(self.disassemble_operand, args))
+        )
+
+    def disassemble_operand(self, value):
+        if 0 <= value <= 32767:
+            if value < 128:
+                return '%s (%s)' % (value, CHARS.get(value, chr(value)))
+            else:
+                return str(value)
+
+        if 32768 <= value <= 32775:
+            return 'R%s' % (value - 32768)
+
+        return '%s (INVALID)' % value
+
 if __name__ == '__main__':
-    vm = VirtualMachine()
-    vm.load('challenge.bin')
+    vm = VirtualMachine(trace=False)
+    code_size = vm.load('challenge.bin')
     vm.execute()
